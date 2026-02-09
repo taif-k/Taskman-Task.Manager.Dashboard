7ce3a3a974ebd655346c9510b1ecb47e66db3585
taif-k <taifk817@gmail.com>
2026-02-09 19:05:49 +0530

    disabled btn

--- src/layout/LeftSideLayout.jsx
+++ src/layout/LeftSideLayout.jsx
@@ -36,27 +36,27 @@ const LeftSideLayout = () => {
           <img src={homeIcon} alt="Home" />
         </Nav.Link>
 
-        <Nav.Link as={NavLink} to="/notification" className="mt-3">
+        <Nav.Link as={NavLink} to="/notification" className="mt-3" disabled>
           <img src={bellIcon} alt="notification" />
         </Nav.Link>
 
-        <Nav.Link as={NavLink} to="/messages" className="mt-3">
+        <Nav.Link as={NavLink} to="/messages" className="mt-3" disabled>
           <img src={msgIcon} alt="messages" />
         </Nav.Link>
 
-        <Nav.Link as={NavLink} to="/bar" className="mt-3">
+        <Nav.Link as={NavLink} to="/bar" className="mt-3" disabled>
           <img src={barIcon} alt="bar" />
         </Nav.Link>
 
-        <Nav.Link as={NavLink} to="/below-bar" className="mt-3">
+        <Nav.Link as={NavLink} to="/below-bar" className="mt-3" disabled>
           <img src={belowBarIcon} alt="below-bar" />
         </Nav.Link>
 
-        <Nav.Link as={NavLink} to="/clock" className="mt-3">
+        <Nav.Link as={NavLink} to="/clock" className="mt-3" disabled>
           <img src={clockIcon} alt="clock" />
         </Nav.Link>
 
-        <Nav.Link as={NavLink} to="/removed-tasks" className="mt-3">
+        <Nav.Link as={NavLink} to="/removed-tasks" className="mt-3" disabled>
           <img src={trashIcon} alt="removed tasks" />
         </Nav.Link>
 
