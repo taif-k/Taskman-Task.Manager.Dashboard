d7dafb27ecaf5476bd8360b264f94f7a1df15c3b
taif-k <taifk817@gmail.com>
2026-03-13 12:33:58 +0530

    cors issue resolved

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,44 @@
+require("dotenv").config()
+
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 5000;
-
 
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+// CORS configuration
+app.use(cors({
+  origin: "https://task-manager-figma.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
+
+// Handle preflight requests
+// app.options("/*", cors());
+
+
+//   Middleware
+
 app.use(express.json());
 
+
+//   Database Connection
 connectDB();
 
+
+//   Routes
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Taskman API running");
 });
 
+
+//   Start Server
 app.listen(PORT, () => {
-  console.log("Server running on port 5000");
+  console.log(`Server running on port ${PORT}`);
 });
