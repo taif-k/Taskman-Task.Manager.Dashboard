503ce7f106c5102bf3a8d87fb02eeb4c84486efd
taif-k <taifk817@gmail.com>
2026-03-14 12:13:11 +0530

    password hashing and user token added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,28 +1,37 @@
 /*
 Purpose: Contains the logic for authentication APIs.
 
-
 Controllers:
-
     receive request data
     interact with models
     send responses
-
 */
 
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+// SIGNUP
 exports.signup = async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
 
+    // check if user exists
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       return res.status(400).json({ message: "Email already registered" });
     }
 
-    const user = new User({ fullName, email, password });
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = new User({
+      fullName,
+      email,
+      password: hashedPassword
+    });
 
     await user.save();
 
@@ -33,29 +42,42 @@ exports.signup = async (req, res) => {
   }
 };
 
-// const User = require("../models/user");
 
+// LOGIN
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // check user
     const user = await User.findOne({ email });
 
     if (!user) {
       return res.status(400).json({ message: "Invalid email or password" });
     }
 
-    if (user.password !== password) {
-      return res.status(400).json({ message: "Invalid email or password" });
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
+
+    // create token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
 
     res.json({
       user: {
         id: user._id,
         fullName: user.fullName,
-        email: user.email,
+        email: user.email
       },
+      token
     });
+
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
